09b1ad774d38f90db846f18e6ff282626ceab4b0
RandumbGuy1 <78370387+RandumbGuy1@users.noreply.github.com>
2021-12-03 16:58:19 -0500

    12/3
    idk

--- Assets/Scripts/AI/FollowPlayer.cs
+++ Assets/Scripts/AI/FollowPlayer.cs
@@ -41,11 +41,10 @@ public class FollowPlayer : MonoBehaviour
 
     void FollowThePlayer()
     {
-        if (enemyRb.transform.parent != transform) enemyRb.transform.SetParent(transform);
-
         HandleRotation();
+        enemyRb.AddForce(Vector3.down * 80f * enemyRb.mass, ForceMode.Acceleration);
 
-        enemyRb.AddForce(Vector3.down * 85f, ForceMode.Acceleration);
+        if (enemyRb.transform.parent != transform) enemyRb.transform.SetParent(transform);
 
         if (agent.path.corners.Length > 0 && agent.isOnOffMeshLink)
         {
@@ -54,27 +53,23 @@ public class FollowPlayer : MonoBehaviour
             agent.gameObject.SetActive(false);
         }
 
+        agent.transform.localPosition = Vector3.zero;
+        agent.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        agent.nextPosition = enemyRb.transform.position;
+
         if (!grounded)
         {
+            agent.gameObject.SetActive(false);
             enemyRb.AddForce((player.position - enemyRb.transform.position).normalized * speed * 0.5f, ForceMode.Acceleration);
             return;
         }
 
-        agent.transform.localPosition = Vector3.zero;
-        agent.transform.localRotation = Quaternion.Euler(Vector3.zero);
-
         float sqrEnemyToPlayer = (player.position - enemyRb.transform.position).sqrMagnitude;
-        if (sqrEnemyToPlayer < standingDistance * standingDistance) return;
-
         agent.gameObject.SetActive(true);
         agent.SetDestination(player.position);
 
         Vector3 pathDir = (agent.path.corners.Length > 1 ? agent.path.corners[1] : player.transform.position) - enemyRb.transform.position;
-      
-        for (int i = 0; i < agent.path.corners.Length - 1; i++)
-        {
-            Debug.DrawLine(agent.path.corners[i], agent.path.corners[i + 1], Color.red);
-        }
+        if (sqrEnemyToPlayer < standingDistance * standingDistance && Vector3.Dot(pathDir.normalized, (player.position - enemyRb.transform.position).normalized) > 0.5f) return;
 
         enemyRb.AddForce(pathDir.normalized * speed * 5f, ForceMode.Acceleration);
     }
@@ -88,20 +83,16 @@ public class FollowPlayer : MonoBehaviour
             Quaternion fromTo = Quaternion.FromToRotation(enemyRb.transform.up, Vector3.up);
             enemyRb.AddTorque(new Vector3(fromTo.x, fromTo.y, fromTo.z) * 100f, ForceMode.Acceleration);
         }
-
-        Vector3 vel = (player.position - enemyRb.transform.position);
-        vel.y = 0f;
     }
 
-    void OnDrawGizmosSelected()
-    {
+    void OnDrawGizmos()
+    { 
         Gizmos.DrawWireCube(enemyRb.position + enemyRb.transform.TransformDirection(groundCheckOffset), groundCheckHalfExtents * 2f);
     }
 
     private void SetGroundCheckInterval()
     {
         Quaternion orientation = Quaternion.LookRotation(enemyRb.transform.up);
-
         grounded = Physics.CheckBox(enemyRb.position + enemyRb.transform.TransformDirection(groundCheckOffset), groundCheckHalfExtents, orientation, Ground);
 
         Invoke("SetGroundCheckInterval", groundCheckInterval);
